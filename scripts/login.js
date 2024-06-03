864bd5d83a38e2c752aa1170ddb7b4b9c7e6765a
CaioGato <caio_h_borba@estudante.sesisenai.org.br>
2024-06-03 12:44:46 -0300

    arrumei dark mode

--- scripts/login.js
+++ scripts/login.js
@@ -31,3 +31,20 @@ function validarFormulario() {
     window.location.href = './login2.html'
     return true;
 }
+
+document.addEventListener('DOMContentLoaded', function darkmode() {
+    const toggleButton = document.getElementById('toggle-button');
+
+    if (localStorage.getItem('dark-mode') === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        
+        const isDarkMode = document.body.classList.contains('dark-mode');
+        localStorage.setItem('dark-mode', isDarkMode);
+    });
+});
